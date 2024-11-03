3e7e0c452818085251fb397842461290a9e663c3
ebe <130290570+akaebe@users.noreply.github.com>
2024-11-03 17:30:23 +0000

    empty

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,7 @@ function App() {
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
+          <h1>hi im ebe</h1>
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
